1d3da7a21ffe61fd59a79308370f24e4a8786a04
Balakumaran Palanivel <kumaraxe@gmail.com>
2018-01-16 23:04:55 +0000

    [MASTER] Computing x,y co ordinates based on mouse location

--- FluidSimulation/scripts/main.js
+++ FluidSimulation/scripts/main.js
@@ -1,10 +1,19 @@
 var mouseOut = false;
 var mouseEnable = false;
+var lastMouseCoordinates =  [0,0];
+var mouseCoordinates =  [0,0];
+
+var actualWidth, actualHeight;
+var body;
 
 window.onload = init;
 
 function init(){
     canvas = document.getElementById("glcanvas");
+    body = document.getElementsByTagName("body")[0];
+
+    actualWidth = Math.round(body.clientWidth);
+    actualHeight = Math.round(body.clientHeight);
 
     window.onmousemove = onMouseMove;
     window.onmousedown = onMouseDown;
@@ -20,7 +29,31 @@ function init(){
 }
 
 function onMouseMove(e){
+    lastMouseCoordinates = mouseCoordinates;
+    // clip the mouse co-ordinates to window for computation
+    var padding = 10;
+    var x = e.clientX;
+    if (x < padding){
+        x = padding;
+    }
 
+    if (x > actualWidth - padding)
+    {
+        x = actualWidth - padding 
+    }
+
+    var y = e.clientY;
+    if (y < padding){
+        y = padding;
+    }
+
+    if (y > actualHeight - padding)
+    {
+        y = actualHeight - padding 
+    }
+
+    // changing the y co-ordinate is from bottom to up
+    mouseCoordinates = [x, actualHeight-y];
 }
 
 function onMouseDown(){
@@ -32,5 +65,5 @@ function onMouseUp(){
 }
 
 function onResize(){
-    
+
 }
