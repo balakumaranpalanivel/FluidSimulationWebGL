29608f5f469b0d84dd685aae20625420ce9750a8
Balakumaran Palanivel <kumaraxe@gmail.com>
2018-01-18 21:36:18 +0000

    [MASTER] Method to create texture

--- FluidSimulation/scripts/shaderUtils.js
+++ FluidSimulation/scripts/shaderUtils.js
@@ -98,9 +98,25 @@ function initShaderUtils(){
         gl.vertexAttribPointer(positionLocation, 2, gl.Float, false, 0, 0);
     }
 
+    function makeTexture(gl, width, height, type, data){
+        var texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+
+        // Set the parameters so we can render any size image.
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, type, data);
+
+        return texture;
+    }
+
     return{
         createProgramFromSource: createProgramFromSource,
         createProgramFromScripts: createProgramFromScripts,
-        loadVertexData: loadVertexData
+        loadVertexData: loadVertexData,
+        makeTexture: makeTexture
     }
 }
